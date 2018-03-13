575dc9ddd75c502c38208ef6f91ec5ce3f802f71
Lisa Lee <snickers495@gmail.com>
2018-03-13 14:23:17 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -39,6 +39,7 @@ function checkWinner(){
   let state = []
   let winner = ""
   squares.forEach(ele => state.push(ele.innerHTML))
+  
   const won = winCombinations.some(ele => {
     if (state[ele[0]] !== "" && state[ele[1]] === state[ele[0]] && state[ele[1]] === state[ele[2]]) {
       winner = state[ele[0]]
