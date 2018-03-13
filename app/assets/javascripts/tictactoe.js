98d3fd4f54fee499fb6c4d5d14f0d1b41fb970d8
Lisa Lee <snickers495@gmail.com>
2018-03-13 14:27:52 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -39,7 +39,7 @@ function setMessage(message){
 function checkWinner(){
   let state = []
   let winner = ""
-   state = $("td").map(function() {
+  let state = $("td").map(function() {
     return $(this).html()
   }).toArray();
   const won = winCombinations.some(ele => {
